b9bbfcb141e9552b43651f2cfc18aec4c939519e
kiclu <kiclu@protonmail.com>
2023-06-13 22:51:20 +0200

    added abs

---
+++ src/simdip/abs.c
@@ -0,0 +1,49 @@
+#include<simdip/abs.h>
+
+/*
+OP_ABS
+absolute value of every pixel in the image
+*/
+
+// simd, absolute value, .bmp, 8 bits per channel, pipeline
+void simd_abs_bmp_8bpc(uint8_t* ptr_r, uint8_t* ptr_g, uint8_t* ptr_b){
+    __m256i va_r = _mm256_load_si256((__m256i*)ptr_r);
+    __m256i va_g = _mm256_load_si256((__m256i*)ptr_g);
+    __m256i va_b = _mm256_load_si256((__m256i*)ptr_b);
+    _mm256_store_si256((__m256i*)ptr_r, _mm256_abs_epi8(va_r));
+    _mm256_store_si256((__m256i*)ptr_g, _mm256_abs_epi8(va_g));
+    _mm256_store_si256((__m256i*)ptr_b, _mm256_abs_epi8(va_b));
+}
+
+inline static int8_t abs(int8_t x){ return x >= 0 ? x : -x; }
+
+// simd, absolute value, .bmp, 8 bits per channel, no pipeline
+void simd_abs_bmp_8bpc_npl(imgfile_t* imgfile){
+    for(size_t i = 0; i < imgfile->height; ++i){
+        uint8_t* ptr_r = imgfile->imgdata._8bpc.r[i];
+        uint8_t* ptr_g = imgfile->imgdata._8bpc.g[i];
+        uint8_t* ptr_b = imgfile->imgdata._8bpc.b[i];
+
+        size_t j = 0;
+        for(; j < (imgfile->width & ~0x1F); j += 32){
+            simd_abs_bmp_8bpc(ptr_r + j, ptr_g + j, ptr_b + j, c);
+        }
+
+        for(; j < imgfile->width; ++j){
+            *ptr_r++ = abs(*ptr_r);
+            *ptr_g++ = abs(*ptr_g);
+            *ptr_b++ = abs(*ptr_b);
+        }
+    }
+}
+
+// no simd, absolute value, .bmp, 8 bits per channel, no pipeline
+void abs_bmp_8bpc_npl(imgfile_t* imgfile){
+    for(size_t i = 0; i < imgfile->height; ++i){
+        for(size_t j = 0; j < imgfile->width; ++j){
+            imgfile->imgdata._8bpc.r[i][j] = abs(imgfile->imgdata._8bpc.r[i][j]);
+            imgfile->imgdata._8bpc.g[i][j] = abs(imgfile->imgdata._8bpc.g[i][j]);
+            imgfile->imgdata._8bpc.b[i][j] = abs(imgfile->imgdata._8bpc.b[i][j]);
+        }
+    }
+}
