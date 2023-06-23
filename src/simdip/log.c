234de1ecb069b6a9a9c0c69d741d2ef9c57829af
kiclu <kiclu@protonmail.com>
2023-06-23 13:59:21 +0200

    log opt

--- src/simdip/log.c
+++ src/simdip/log.c
@@ -7,37 +7,55 @@ replace every pixel of image with its logarithm
 
 // https://en.wikipedia.org/wiki/Fast_inverse_square_root
 
+#define fva_r(x) _mm256_cvtepi32_ps(_mm256_cvtepu8_epi32(_mm_loadu_si64(ptr_r+8*(x))))
+#define fva_g(x) _mm256_cvtepi32_ps(_mm256_cvtepu8_epi32(_mm_loadu_si64(ptr_g+8*(x))))
+#define fva_b(x) _mm256_cvtepi32_ps(_mm256_cvtepu8_epi32(_mm_loadu_si64(ptr_b+8*(x))))
+
 // simd, log, 8 bits per channel, pipeline
 void simd_log_8bpc(uint8_t* ptr_r, uint8_t* ptr_g, uint8_t* ptr_b){
-    for(size_t k = 0; k < 32; k += 8){
-        __m256 fva_r = _mm256_setr_ps(
-            ptr_r[k+0], ptr_r[k+1], ptr_r[k+2], ptr_r[k+3],
-            ptr_r[k+4], ptr_r[k+5], ptr_r[k+6], ptr_r[k+7]
-        );
+    __m256i c = _mm256_set1_epi32(127);
 
-        __m256 fva_g = _mm256_setr_ps(
-            ptr_g[k+0], ptr_g[k+1], ptr_g[k+2], ptr_g[k+3],
-            ptr_g[k+4], ptr_g[k+5], ptr_g[k+6], ptr_g[k+7]
-        );
+    // evil floating point bit hack
+    __m256i logva_r[] = {
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_r(0), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_r(1), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_r(2), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_r(3), 23), c)
+    };
 
-        __m256 fva_b = _mm256_setr_ps(
-            ptr_b[k+0], ptr_b[k+1], ptr_b[k+2], ptr_b[k+3],
-            ptr_b[k+4], ptr_b[k+5], ptr_b[k+6], ptr_b[k+7]
-        );
+    __m256i logva_g[] = {
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_g(0), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_g(1), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_g(2), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_g(3), 23), c)
+    };
 
-        __m256i c = _mm256_set1_epi32(127);
+    __m256i logva_b[] = {
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_b(0), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_b(1), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_b(2), 23), c),
+        _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_b(3), 23), c)
+    };
 
-        // evil floating point bit hack
-        __m256i logva_r = _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_r, 23), c);
-        __m256i logva_g = _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_g, 23), c);
-        __m256i logva_b = _mm256_sub_epi32(_mm256_srli_epi32((__m256i)fva_b, 23), c);
+    __m256i vres_r = _mm256_packus_epi16(
+        _mm256_permute4x64_epi64(_mm256_packus_epi32(logva_r[0], logva_r[2]), 0xD8),
+        _mm256_permute4x64_epi64(_mm256_packus_epi32(logva_r[1], logva_r[3]), 0xD8)
+    );
 
-        for(size_t i = 0; i < 8; ++i){
-            ptr_r[k+i] = *((uint8_t*)&logva_r + (i << 2));
-            ptr_g[k+i] = *((uint8_t*)&logva_g + (i << 2));
-            ptr_b[k+i] = *((uint8_t*)&logva_b + (i << 2));
-        }
-    }
+    __m256i vres_g = _mm256_packus_epi16(
+        _mm256_permute4x64_epi64(_mm256_packus_epi32(logva_g[0], logva_g[2]), 0xD8),
+        _mm256_permute4x64_epi64(_mm256_packus_epi32(logva_g[1], logva_g[3]), 0xD8)
+    );
+
+    __m256i vres_b = _mm256_packus_epi16(
+        _mm256_permute4x64_epi64(_mm256_packus_epi32(logva_b[0], logva_b[2]), 0xD8),
+        _mm256_permute4x64_epi64(_mm256_packus_epi32(logva_b[1], logva_b[3]), 0xD8)
+    );
+
+    _mm256_store_si256((__m256i*)ptr_r, vres_r);
+    _mm256_store_si256((__m256i*)ptr_g, vres_g);
+    _mm256_store_si256((__m256i*)ptr_b, vres_b);
+
 }
 
 #include<math.h>
