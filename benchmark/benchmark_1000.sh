fcc571e9d1027d76849b59503db32381782291f9
kiclu <kiclu@protonmail.com>
2023-06-26 02:41:18 +0200

    updated benchmark scripts

--- benchmark/benchmark_1000.sh
+++ benchmark/benchmark_1000.sh
@@ -3,13 +3,13 @@ sum_s3=0
 
 for i in {1..1000}
 do
-    cur_s0=$(../aor2p $@ -s0 | grep 'time taken' | awk '{print $3}' | rev | cut -c 3- | rev)
+    cur_s0=$(./aor2p $@ -s0 | grep 'time taken' | awk '{print $3}' | rev | cut -c 3- | rev)
     sum_s0=$(( $cur_s0 + $sum_s0 ))
 done
 
 for i in {1..1000}
 do
-    cur_s3=$(../aor2p $@ -s3 | grep 'time taken' | awk '{print $3}' | rev | cut -c 3- | rev)
+    cur_s3=$(./aor2p $@ -s3 | grep 'time taken' | awk '{print $3}' | rev | cut -c 3- | rev)
     sum_s3=$(( $cur_s3 + $sum_s3 ))
 done
 
--- benchmark/benchmark_1000s0.sh
+++ benchmark/benchmark_1000s0.sh
@@ -1,4 +1,4 @@
 for i in {1..1000}
 do
-    cur_s3=$(../aor2p $@ -s0)
+    cur_s3=$(./aor2p $@ -s0)
 done
